757fcf2b01e7774241acbed05be796eb31123519
JOSEPHONYANGO <joseph.onyango@student.moringaschool.com>
2022-03-13 22:06:39 +0300

    jquery functions added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
-    
+
 })
+
+
+
+$(document).ready(function() {
+    $(".design").click(function() {
+      $(".design").toggle();
+      $(".para-one").toggle();
+    });
+  });
+
+  
